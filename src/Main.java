a257b82d83622d131dea102c54d3d2a51d59d42a
Wendy Wu <96844488+ww-classy-lady@users.noreply.github.com>
2024-09-25 23:14:06 -0400

    basically finished everything else
    basically added in exceptions to encrypt and decrypt. Add, read functionality. Correct flow, ...etc

--- src/Main.java
+++ src/Main.java
@@ -32,20 +32,29 @@ public class Main {
     // String decrypt(key, encryptedBase64EncodedText), returns clear text
 
     private static String encrypt(SecretKeySpec key, String plaintext) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.ENCRYPT_MODE, key);
+        try{
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, key);
 
-        byte[] encryptedData = cipher.doFinal(plaintext.getBytes());
-        return new String(Base64.getEncoder().encode(encryptedData));
+            byte[] encryptedData = cipher.doFinal(plaintext.getBytes());
+            return new String(Base64.getEncoder().encode(encryptedData));
+        } catch(Exception e){
+            System.out.println("ERROR IN ENCRYPT: " + e.getMessage());
+        }
+        return null;
     }
 
     private static String decrypt(SecretKeySpec key, String encryptedBase64EncodedText) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.DECRYPT_MODE, key);
-
-        byte[] decoded = Base64.getDecoder().decode(encryptedBase64EncodedText);
-        byte[] decrypted = cipher.doFinal(decoded);
-        return new String(decrypted);
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] decoded = Base64.getDecoder().decode(encryptedBase64EncodedText);
+            byte[] decrypted = cipher.doFinal(decoded);
+            return new String(decrypted);
+        }catch (Exception e) {
+            System.out.println("ERROR in DECRYPT: " + e.getMessage());
+        }
+        return null;
     }
 
     private static SecretKeySpec createKey(String new_passphrase, byte[] salt) throws InvalidKeySpecException, NoSuchAlgorithmException {
@@ -60,9 +69,22 @@ public class Main {
     private static void writeToFile(File file, String left, String right) throws IOException {
         FileWriter fr = new FileWriter(file, true);
         fr.write(left + ":" + right);
+        fr.write("\n");
         fr.close();
     }
-
+    private static String readFromFile(File file, SecretKeySpec key, String label) throws IOException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+        BufferedReader br = new BufferedReader(new FileReader("src/passwords.txt"));
+        String line = br.readLine();
+        while(line != null) {
+            if (line.substring(0, line.indexOf(":")).equals(label)) {
+                String encryptedPassword = line.substring(line.indexOf(":") + 1);
+                return decrypt(key, encryptedPassword);
+            }
+            line = br.readLine();
+        }
+        br.close();
+        return null;
+    }
     // private static String readLine(file, )
     public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, IOException {
         //Main menu
@@ -70,70 +92,21 @@ public class Main {
         System.out.print("Enter the passcode to access your passwords: ");
         String passphrase = scanner.nextLine();
         File file = new File("src/passwords.txt");
+        SecretKeySpec key;
 
-        if (file.exists()) {
-            System.out.println("File exists");
-            // extract salt
-            // make new key
-            // use key to decrypt token
-            // check if token is same
-            BufferedReader br = new BufferedReader(new FileReader("src/passwords.txt"));
-            String firstLine = br.readLine();
-            String encodedSalt = firstLine.substring(0, firstLine.indexOf(":"));
-            byte[] salt = Base64.getDecoder().decode(encodedSalt);
-            SecretKeySpec key = createKey(passphrase, salt);
-            String encodedToken = firstLine.substring(firstLine.indexOf(":")+1);
-            String decryptedToken = decrypt(key, encodedToken);
-
-            if (token.equals(decryptedToken)){
-                while(true) {
-                    System.out.println("a: Add Password");
-                    System.out.println("r: Read Password");
-                    System.out.println("q. Quit");
-                    System.out.print("Enter choice: ");
-                    String choice = scanner.nextLine();
-                    switch(choice) {
-                        case "a":
-                            System.out.print("Enter label for password: ");
-                            String label = scanner.nextLine();
-                            System.out.print("Enter password to store: ");
-                            String password = scanner.nextLine();
-                            break;
-                        case "r":
-                            System.out.print("Enter label for password: ");
-                            String label2 = scanner.nextLine();
-                            //TODO: finding passcode from file based on provided label
-                            // if label does not exist, return not found or some error
-                            System.out.println("Found: //TODO! ");
-                            break;
-                        case "q":
-                            System.out.println("Quitting");
-                            System.exit(0);
-                    }
-                }
-            } else {
-            }
-
-
-        } else {
+        if (!file.exists()) {
             System.out.println("No password file detected. Creating a new password file.");
             try {
                 file.createNewFile();
-                System.out.println("File Created");
-
                 SecureRandom random = new SecureRandom();
                 byte[] salt = new byte[16];
                 random.nextBytes(salt);
                 String encodedSalt = Base64.getEncoder().encodeToString(salt);
 
-                System.out.print("Enter your new passphrase: ");
-                String new_passphrase = scanner.nextLine();
-
-                SecretKeySpec key = createKey(new_passphrase, salt);
+                key = createKey(passphrase, salt);
                 String encryptedToken = encrypt(key, token);
 
                 writeToFile(file, encodedSalt, encryptedToken);
-
 
                 // Take the key, encrypt the token
                 // Store the salt and the token in the first line
@@ -142,5 +115,57 @@ public class Main {
                 throw new RuntimeException(e);
             }
         }
+        else {
+            //file exists now
+            BufferedReader br = new BufferedReader(new FileReader("src/passwords.txt"));
+            String firstLine = br.readLine();
+            String encodedSalt = firstLine.substring(0, firstLine.indexOf(":"));
+            byte[] salt = Base64.getDecoder().decode(encodedSalt);
+            key = createKey(passphrase, salt);
+            String encodedToken = firstLine.substring(firstLine.indexOf(":") + 1);
+            String decryptedToken = decrypt(key, encodedToken);
+            if (!token.equals(decryptedToken)) {
+                //User entered wrong password that does not successfully decrypt our token to its original form/value
+                System.out.println("Error: The password you have entered is incorrect.");
+                System.exit(0);
+            }
+        }
+        //file does not exist but we created a new file
+        //or file exists and token equals decrypted token
+        while(true) {
+            System.out.println("a: Add Password");
+            System.out.println("r: Read Password");
+            System.out.println("q. Quit");
+            System.out.print("Enter choice: ");
+            String choice = scanner.nextLine();
+            switch(choice) {
+                case "a":
+                    System.out.print("Enter label for password: ");
+                    String label = scanner.nextLine();
+                    System.out.print("Enter password to store: ");
+                    String password = scanner.nextLine();
+                    String encryptedPass = encrypt(key, password);
+                    writeToFile(file, label, encryptedPass);
+                    System.out.println("Successful ADD operation - Password stored");
+                    break;
+                case "r":
+                    System.out.print("Enter label for password: ");
+                    String label2 = scanner.nextLine();
+                    //TODO: finding passcode from file based on provided label
+                    // if label does not exist, return not found or some error
+                    String decryptedPass = readFromFile(file, key, label2);
+                    if(decryptedPass == null) {
+                        System.out.println("Failed READ operation - Password not found with this label");
+                    }
+                    else {
+                        System.out.println("Found: " + decryptedPass);
+                    }
+                    break;
+                case "q":
+                    System.out.println("Quitting");
+                    System.exit(0);
+            }
+        }
+
     }
 }
